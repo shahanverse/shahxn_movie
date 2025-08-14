8a7f84f1b3e9045cecf85e2f34a8b302cb54f553
shahanverse <shahanverse@gmail.com>
2025-08-15 02:00:12 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/shahxn_movie">
+    <HashRouter basename="/shahxn_movie">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
