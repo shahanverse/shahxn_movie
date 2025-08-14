4f42a01cf34f879abcffd021f3d35860cb36cc1e
shahanverse <shahanverse@gmail.com>
2025-08-15 01:54:35 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,12 +2,12 @@ const API_KEY= 'f575695e6371b7b6dea47dc29af9439c';
 const BASE_URL= 'https://api.themoviedb.org/3';
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
   const data = await response.json();
   return data.results;
 };
 export const searchMovies = async (query) => {
-  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&query==${encodeURIComponent(query)}&page=1`);
   const data = await response.json();
   return data.results;
 };
